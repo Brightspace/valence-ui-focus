f0562c9a1ce49352436bb2e741217cc7759221b7
Dave Batiste <dave.batiste@d2l.com>
2014-09-17 11:54:58 -0400

    Adding mixin for focus-outline.

---
+++ test/unit/focusSpec.js
@@ -0,0 +1,24 @@
+( function() {
+	'use strict';
+
+	describe( 'vui-focus', function() {
+
+		var node;
+
+		beforeEach( function () {
+
+		} );
+
+		afterEach( function() {
+
+		} );
+
+		it( 'test1', function() {
+
+			expect( true ).toBeTruthy();
+
+		} );
+
+	} );
+
+} )();
