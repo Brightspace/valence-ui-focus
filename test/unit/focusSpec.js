a94b660d4924b9cbfed1fcd37645c84c60ebd7d8
Dave Batiste <dave.batiste@d2l.com>
2014-09-18 17:11:58 -0400

    Updating some tests for focus styling.

--- test/unit/focusSpec.js
+++ test/unit/focusSpec.js
@@ -7,15 +7,31 @@
 
 		beforeEach( function () {
 
+			jasmine.addMatchers( customMatchers );
+
+			node = document.body.appendChild( document.createElement( 'div' ) );
+			node.className = 'vui-outline-force';
+
 		} );
 
 		afterEach( function() {
 
+			document.body.removeChild( node );
+
 		} );
 
-		it( 'test1', function() {
+		it( 'defines a "vui-outline:focus" selector', function() {
+			expect( document ).toHaveCssSelector( '.vui-outline:focus' );
+		} );
 
-			expect( true ).toBeTruthy();
+		it( 'defines a "vui-outline-force" selector', function() {
+			expect( document ).toHaveCssSelector( '.vui-outline-force' );
+		} );
+
+		it( 'should have default outline when not focused', function() {
+
+			var style = window.getComputedStyle( node, null );
+			expect( style.getPropertyValue( 'outline-color' ) ).toBe( 'rgb(148, 204, 218)' );
 
 		} );
 
